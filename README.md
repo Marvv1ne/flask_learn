14753a351ef6aa8d42b460ceadd045f8fcd89eb2
Gura-san <gorn89@inbox.ru>
2024-12-22 19:03:28 +0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # flask_learn
-# flask_learn
---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+@app.route('/users', methods=['GET', 'POST'])
+def users():
+    if request.method == 'POST':
+        return 'Hello from POST /users'
+    return 'Hello from GET /users'
